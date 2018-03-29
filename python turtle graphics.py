1b7fbf252f1c45f9c5f6d4e94a421f29bba46d75
srisushant1234 <32979303+srisushant1234@users.noreply.github.com>
2018-03-29 18:00:38 +0530

    Add files via upload

---
+++ python turtle graphics.py
@@ -0,0 +1,26 @@
+import turtle
+for steps in range(10000000000000000000):
+    for steps in range(4):
+        turtle.color('green')
+        turtle.forward(100)
+        turtle.right(90)
+    for steps in range(4):
+        turtle.color('red')
+        turtle.forward(100)
+        turtle.left(90)
+    for steps in range(4):
+        turtle.color('blue')
+        turtle.forward(100)
+        turtle.right(90)    
+    for steps in range(4):
+        turtle.color('yellow')
+        turtle.forward(100)
+        turtle.left(90)
+    for steps in range(4):
+        turtle.color('white')
+        turtle.forward(100)
+        turtle.right(90)
+    for steps in range(4):
+        turtle.color('pink')
+        turtle.forward(100)
+        turtle.left(90)   
